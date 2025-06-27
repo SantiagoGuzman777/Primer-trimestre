ee5cabe73dbe6ab5ae208cfcb647c7286e7d74f5
Santiago Guzman <santiagoguz111@gmail.com>
2025-06-27 17:05:11 -0500

    primer repositorio

---
+++ actividad_1.py
@@ -0,0 +1,341 @@
+# nombre=input("¿Cual es tu nombre?")
+# print(f"su nombre es {nombre}")
+
+#------------------------------------------ejercicio1----------------------------------------------
+# nombre=input("ingree su nombre: ")
+# saludo=input("ingrese un saludo: ")
+# print(f"el nombre ingresado es {nombre} y el saludo ingresado es {saludo}")
+
+#------------------------------------------ejercicio2----------------------------------------------
+# ciudad=input("ingrese una ciudad: ")
+# print (f"la ciudad ingresada es {ciudad}")
+#------------------------------------------ejercicio3----------------------------------------------
+
+# nombre= input("ingrese su nomobre: ")
+# ciudad=input ("ingrese su ciudad: ")
+# print(f"hola me llamo {nombre} y vivo en {ciudad} ")
+#------------------------------------------ejercicio4----------------------------------------------
+# color=input("ingrese su color favorito: ")
+# print(f"su color favorito es {color}")
+#------------------------------------------ejercicio5----------------------------------------------
+# color=input("ingrese su color favorito: ")
+# print(f"su color favorito es {color}")
+#------------------------------------------ejercicio6----------------------------------------------
+# animal= "gato"
+# animal= "perro"
+# print (animal)
+#------------------------------------------ejercicio7----------------------------------------------
+# presentacion= input("ingrese su nombre: ")
+
+# print(f"{presentacion},{presentacion2}")
+#------------------------------------------ejercicio8----------------------------------------------
+# nombre=input("ingrese su nombre: ")
+# ciudad=input("ingrese su ciudad: ")
+# edad=int(input("ingrese su edad: "))
+# apellido=input("ingrese su apellido: ")
+# print (f"{nombre},{ciudad},{edad},{apellido}")
+#------------------------------------------ejercicio9----------------------------------------------
+#texto="""Hamas y la "promocion de la democracia" Noam Chomsky
+
+#Hamas gano combinando una fuerte6s resistencia contra la ocupacion militar con la creacion de organizaciones sociales de base y de servicio a los pobres, una plataforma y una practica que probablemente haria ganar votos en cualquier lugar. La victoria electoral de Hamas es ominosa pero comprensible, a la luz de los acontecimientos. Es enteramente justo describir a Hamas como fundamentalista, extremista y violentista, y como una seria amenaza a la paz y a un acuerdo politicamente justo. Sin embargo, es útil recordar que en aspectos importantes, Hamas no es tan extremista como otros. Por ejemplo, declara que estaría de acuerdo con una tregua con Israel sobre la base de la frontera reconocida a nivel internacional antes de la guerra arabe-israeli de junio de l967. ..
+
+#La posición de Washington hacia las elecciones en Palestina ha sido coherente. Las elecciones fueron postergadas hasta la muerte de Yasser Arafat, que fue recibida como una oportunidad para la realización de la "visión" de Bush sobre un eventual Estado palestino democrativo, que es una palido y vago reflejo del consenso internacional sobre una acuerdo de dos entidades estatales en la zona, que Estados Unidos viene bloqueando desde hace 30 años....
+
+# El compromiso formal de Hamas de "destruir Israel" lo pone a la par con Estados Unidos e Israel, que prometieron por mucho tiempo que no habria ningun "Estado palestino adicional" (aparte de Jordania", hasta que ambas naciones aflojaron parcialmente su posicion, para aceptar un mini Estado constituido por los fragmentos que queden despues que Israel se apropie de todas las partes de Palestina que desea....
+
+# Simplemente como conjetura, imagine el lector una inversion de las circunstancias: que Hamas permitiese a los israelies vivir en cantones desparramados e invariables, virtualmente separados unos de otros, y en alguna pequeña parte de Jerusalen, mientras los palestinos construyen enormes asentamientos y proyectos de infraestructura para apoderarse de las tierras y los recursos de Israel, Y que, ademas Hamas acepte llamar a esos fragmentos "un Estado". Si se hicieran propuestas para esta empobrecida "categoria de Estado", nosotros nos sentiriamos, con razon, horrorizados. Pero con ese tipo de propuestas, la posicion de Hamas seria esencialmente igual a la de Estados Unidos e Israel."""
+# buscar=texto.find("imagine")
+# print (buscar)
+# buscar1=texto.find("Israel")
+# print(buscar1)
+# extraccion=texto[1720:789]
+# print (extraccion)
+#texto="""Doña Uzeada de Ribera Maldonado de Bracamonte y Anaya era baja, rechoncha, abigotada. Ya no existia razon para llamar talle al suyo. Sus colores vivos, sanos, podian mas que el albayalde y el soliman del afeite, con que se blanqueaba por simular melancolias. Gastaba dos parches oscuros, adheridos a las sienes y que fingian medicamentos. Tenia los ojitos ratoniles, maliciosos. Sabia dilatarlos duramente o desmayarlos con recato o levantarlos con disimulo. Caminaba contoneando las imposibles caderas y era dificil, al verla, no asociar su estampa achaparrada con la de ciertos palmipedos domesticos. Sortijas celestes y azules le ahorcaban las falanges"""
+# buscar=texto.find("maliciosos")
+# buscar1=texto.find("achaparrada")
+# extraer=texto[367:550]
+# print (f"{buscar},{buscar1}")
+# print (extraer)
+# #--------------------------------------------------------------------------------------------
+# buscar=texto.find("medicamentos")
+# buscar1=texto.find("estampa")
+# extraer=texto[325:542]
+# print (f"{buscar},{buscar1}")
+# print (extraer)
+# #-----------------------------------------------------------------------------------------
+# buscar=texto.find("llamar")
+# buscar1=texto.find("desmayarlos")
+# extraer=texto[111:408]
+# print (f"{buscar},{buscar1}")
+# print (extraer)
+#--------------------------------------------------------------------------------------------
+"""num=input("ingresar nombre del eequipo de futbol: ")
+equipo= int(input("Cuntos puntos : "))
+equipo1= int(input("Cuntos puntos tuvo en el segundo semestre "))
+equipo2= int(input("Cuntos puntos tuvo en el tercer semestre "))
+suma=equipo+equipo1+equipo2
+promedio=suma/3
+print(f"el promedio del equipo es {promedio}, del equipo {num})"""
+#------------------------------------------------------------
+# t1="Python"
+# t2="lenguaje"
+# t3="progrmacion"
+# t4="vesatil"
+# print(f"{t1} es un {t2} de {t3} {t4}")
+# frutas=[]
+# frutas1=input("scriba otra fruta")
+# frutas2=input("escriba otrasfrutas")
+# frutas3=input("escriba la ultima fruta")
+# frutas.append(frutas1)
+# frutas.append(frutas2)
+# frutas.append(frutas2)
+# print (frutas)
+
+#------------------------ejercicio 2------------------
+
+# edad=[]
+# edad2=int(input("ingrese una edad: "))
+# edad3=int(input("ingrese otra edad: "))
+# edad.append(edad2)
+# edad.append(edad3)
+# print(edad)
+
+#---------------------ejercicio 3-----------------------------
+# nota=[]
+# nota1=float(input("ingrese su nota de matemaicas"))
+# nota2=float(input("ingrese su nota de musica"))
+# suma=nota1+nota2
+# promedio=suma/2
+# nota.append(nota1)
+# nota.append(nota2)
+# print(nota)
+# print(f"el promedio de las notas es {promedio}")
+
+#--------------------ejercicio 4----------------------------
+
+# productos=[]
+# productos1=input("ingrese el primer producto: ")
+# productos2=input("ingrese el segundo producto: ")
+# productos3=input("ingrese el tercer producto: ")
+# productos.append(productos1)
+# productos.append(productos2)
+# productos.append(productos3)
+# print(productos)
+
+#-------------------------ejercicio 5-------------------------------
+# precios=[]
+# articulo1=float(input("ingrese el precio del articculo: "))
+# articulo2=float(input("ingrese el precio del articculo: "))
+# articulo3=float(input("ingrese el  precio del articculo: "))
+# operacion=articulo1+articulo2+articulo3
+# precios.append(articulo1)
+# precios.append(articulo2)
+# precios.append(articulo3)
+# print(precios)
+# print(f"la suma de los articulos es {operacion}")
+
+#------------------------ejercicio 6----------------------------------
+# numero=[]
+# num1=int(input("ingrese el primer numero: "))
+# num2=int(input("ingrese el segundo numero: "))
+# num3=int(input("ingrese el  tercer nuemero: "))
+# num4=int(input("ingrese el cuarto numero: "))
+# numero.append(num1)
+# numero.append(num2)
+# numero.append(num3)
+# numero.append(num4)
+# print(f"el numero mayor fue {max{numeros}} y en numero menor fue {min{numero}}")
+
+#-------------------------ejercicio 7-------------------------------
+
+# nombres=[]
+# nombre1=float(input("ingrese el primer nombre: "))
+# nombre2=float(input("ingrese el segundo nombre: "))
+# nombre3=float(input("ingrese el tercer nombre: "))
+# nombres.append(nombre1)
+# nombres.append(nombre2)
+# nombres.append(nombre3)
+# print(f"hola {nombre1} y {nombre2}")
+
+#-----------------------ejercicio 8-----------------------------
+
+# temperatura=[]
+# temperatura1=float(input("ingrese la temperatura: "))
+# temperatura2=float(input("ingrese la temperatura: "))
+# temperatura.append(temperatura1)
+# temperatura.append(temperatura2)
+# operacion=temperatura1*9/5+32
+# operacion2=temperatura2*9/5+32
+# print(f" el resultado de la primera temperatura es farenheit es {operacion} y la segunda es {operacion2}")
+
+#------------------ejercicio frutas-------------------------
+
+# frutas=["manzana" ,"naranja","banano"]
+# frutas.remove("banano")
+# print(frutas)
+
+#-------------------ejerciico 1.1----------------------------
+
+# numero=[]
+# numero.append
+# print(numero)
+
+#--------------------------------ejercicio 1.2 --------------------------------------------
+
+# nombres=["Ana","Luis"]
+# nombres.append("Carlos"
+# print(nombres)
+
+#-------------------------------ejercicio 2.1----------------------------
+
+# numeros=["1","2","3"]
+# numeros.insert(0,"99")
+# print(numeros)
+
+#---------------------------------ejercicio 2.2---------------------------------
+# colores=["azul","verde"]
+# colores.insert("rojo")
+# print(colores)
+#----------------------------------ejercicio 3.1---------------------------
+# lista=[1,2,3]
+# lista.extend([4,5,6])
+# print(lista)
+#-----------------------------------ejericio 3.2-------------------------------
+# letras=(["a","b","c"])
+# letras-extend(["ok"])
+# print (letras)
+#-----------------------------------ejercicio 4.1---------------------------------------
+# frutas=(["manzana","uva","pera"])
+# frutas.remove("uva")
+# print(frutas)
+#------------------------------------ejercicio 4.2-------------------------------------
+# lista=[1,2,3,4]
+# valor=lista.pop()
+# print(valor)
+# print(lista)
+#--------------------------------ejercicio 5.1--------------------------------------------
+# lista=["uno","dos","tres"]
+# valor=lista.pop(0)
+# print(valor)
+# print(lista)
+#---------------------------------ejericio 5.2----------------------------------------------
+# lista=[1,2,3]
+# lista.clear()
+# print (lista)
+#---------------------------------ejercicio 6.1----------------------------------------
+# lista=[1,2,3,4,5]
+# lista.clear()
+# print(lista)
+#-------------------------------ejercicio 6.2---------------------------------------------------
+# frutas=["manzana","pera","uva"]
+# print(frutas.index("pera"))
+#-------------------------------ejercicio 7.1-------------------------------------------
+# numeros=[4,5,6,7]
+# print(numeros.index(6))
+#----------------------------ejercicio 7.2--------------------------------------------------
+# numeros=[3,1,3,5,3]
+# print(numeros.count(3))
+#-------------------------------ejercicio 8.1---------------------------------------------
+# lista=["a","b","a","c","a"]
+# print (lista.count("a"))
+#-----------------------------ejercicio 8.2-----------------------------------------------
+# lista=[5,1,4,2,3]
+# lista.sort()
+# print(lista)
+#------------------------------ejercicio 9.1------------------------------------------------------
+# lista=["z","a","m","b"]
+# lista.sort()
+# print(lista)
+#--------------------------------ejercicio9.2--------------------------------------------------------
+# lista=[1,2,3,4]
+# lista.reverse()
+# print(lista)
+#----------------------------------ejercicio 10.1--------------------------------------------
+# lista=["inicio","medio","fin"]
+# lista.reverse()
+# print(lista)
+#-----------------------------------ejercicio 10.2-------------------------------------------
+# lista=[1,2,3]
+# nueva=lista.copy()
+# nueva.append(lista)
+# print(nueva)
+#---------------------------------------ejercicio 11.1---------------------------------------
+# lista=["a","b","c"]
+# nueva=lista.copy()
+# nueva.append(lista)
+# print(lista)
+# print(nueva)
+#------------------------------------ejercicio 11.2-----------------------------------------
+# lista=[1,2,3]
+# nueva=lista.copy()
+# nueva.append(lista)
+# print(lista)
+# print(nueva)
+#---------------------------------actividad------------------------------------------------
+# lista1=[]
+# lista2=[]
+# lista1.append(100)
+# lista1.append("Hola Mundo")
+# lista2.append(300)
+# lista2.append("Hola y Adios")
+# lista3= lista1.copy()
+# lista3.remove("Hola Mundo")
+# lista4=lista2.copy()
+# lista4.remove("Hola y Adios")
+# lista4.remove(300)
+# lista5= []
+# lista5.extend(lista4)
+# lista5.extend(lista3)
+# print (f"lista 1: {lista1}")
+# print (f"lista 2: {lista2}")
+# print (f"lista 3: {lista3}")
+# print (f"lista 4: {lista4}")
+# print (f"lista 5: {lista5}")
+#------------------------tuplas------------------------------------------
+# mi_tupla= (1,2,3)
+# mi_lista= list(mi_tupla) #tupla a lista
+# print (mi_lista)
+# mi_lista=[4,5,6]
+# mi_tupla=tuple(mi_lista)
+# print (mi_tupla)
+#---------------------------------------------------------------------
+# tupla=("manzana","pera")
+# lista= list(tupla)
+# fruta=input("ingrese una fruta: ")
+# lista.append (fruta)
+# tupla=tuple (lista)
+# print (f"Tu lista seria la siguiente: {tupla}")
+#-----------------------------------------------------------------
+# tupla=(4.2,3.8)
+# print (tupla)
+# lista=list(tupla)
+# nota=input("ingrese la tercera nota: ")
+# lista.append (nota)
+
+# tupla=tuple (lista)
+# print (f"Tu tercera nota es {nota}")
+#--------------------------------------------ejercicio2----------------------------
+# tupla=("Ana","Gomez")
+# print (tupla)
+# lista=list(tupla)
+# documento=int(input("ingrese su numero de documento: "))
+# lista.append(documento)
+# tupla=tuple(lista)
+# print (tupla)
+#--------------------------------ejercicio 3---------------------------------------
+#-------------------------ejercicios practicos 1--------------------------------------
+# tupla=(1,2,3,4,5)
+# #ejercicio2
+# print(tupla[2])
+# #ejercicio3
+# longitud=len(tupla)
+# print(longitud)
+# #ejercicio4
+# print (tupla.index(4))
+# #ejercicio5
+# print (tupla.count(2))
+# #ejercicio6
+# tupla2=("Hola mundo",15,1,10)
+# tupla3=(1,2,3, tupla2)
+# #ejercici07
+# print(tupla3[3][0])
---
+++ diccionarios.py
@@ -0,0 +1,99 @@
+#---------------------------DICCIONARIOS-----------------------------------------
+# persona={
+
+# "nombre":"Juan",
+# "edad": 30,
+# "lugarnacimiento":"Palmira"
+
+# }
+
+
+# auto={
+#   "marca":"ford",
+#   "modelo" : "mustang",
+#   "año": 2022,
+#   "color":"azul",
+#   "placa":"PLQ-18D",
+# }
+# #--------------------------------------ACCEDER A VALORES------------------------------------
+# print(auto["modelo"]) #resultado: mustang
+# #--------------------------------------MODIFICAR VALORES-------------------------------------
+# auto[año] = 2023
+# #--------------------------------------AÑADIR NUEVOS ELEMENTOS-----------------------------
+# auto["color"]="rojo"
+# #------------------------------------ELIMINAR ELEMENTOS-------------------------
+# del auto["modelo"]
+ 
+# auto.pop("marca")
+# 
+#------------------------------TALLER-------------------------------------------
+# nota1=float(input("ingrese la primera nota: "))
+# nota2=float(input("ingrese la segunda nota: "))
+# nota3=float(input("ingrese la tercera nota: "))
+# lista=[]
+# lista.append(nota1)
+# lista.append(nota2)
+# lista.append(nota3)
+# print (lista)
+# suma=nota1+nota2+nota3
+# promedio=suma/3
+# print(f"El promedio de sus calificaciones es {promedio}")
+ 
+#------------------------------EJERCICIO2------------------------------------------------
+
+# productos= {
+# "leche":5000,
+# "pan":5000,
+# "huevos":700
+# }
+# aumento=float(input("Que porcentaje deseas aumentar: "))
+# productos["leche"]+=productos["leche"] * (aumento/ 100)
+# productos["pan"]+=productos["pan"] * (aumento/ 100)
+# productos["huevos"]+=productos["huevos"] * (aumento/ 100)
+# print(productos)
+
+#-------------------------------EJERCICIO3-------------------------------------
+
+
+# temperatura1=float(input("ingrese la temperatura del primer dia: "))
+# temperatura2=float(input("ingrese la temperatura del segundo dia: "))
+# temperatura3=float(input("ingrese la temperatura del tercer dia: "))
+# temperatura4=float(input("ingrese la temperatura del cuarto dia: "))
+# temperatura5=float(input("ingrese la temperatura del quinto dia: "))
+
+# celcius=(temperatura1),(temperatura2),(temperatura3),(temperatura4),(temperatura5)
+
+# operacion1= temperatura1* 9/5+32
+# operacion2= temperatura2* 9/5+32
+# operacion3=temperatura3* 9/5+32
+# operacion4=temperatura4* 9/5+32
+# operacion5=temperatura5* 9/5+32
+
+# fahrenheit=(operacion1),(operacion2),(operacion3),(operacion4),(operacion5)
+
+# print("temperatura en °C:", celcius)
+# print("temperatura en °F:", fahrenheit)
+
+
+#---------------------------------EJERCICIO 4-------------------------------
+# edades= [int(input("edad1:")),int(input("edad2:")),int(input("edad3:")),int(input("edad4:")),int(input("edad5:"))]
+# promedio=sum(edades)/len(edades)
+# print(f"Mayor: {max(edades)},Menor: {min(edades)}, promedio: {promedio}")
+#----------------------------------EJERCICIO 5---------------------------------------------
+
+frutas= {
+"Uva": "El precio por kilo es 2000",
+"Limon":"El precio por kilo es 3000",
+"Naranja": "El precio por kilo es 4000"
+}
+
+print(frutas)
+
+
+
+
+
+
+
+
+
